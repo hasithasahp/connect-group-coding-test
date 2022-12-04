c393673c98d477d4d434f92ea879285f29c26063
Hasitha Samarasinghe <hasitha.h45h@gmail.com>
2022-12-04 18:21:57 +0530

    Added Seeder and Model for Locations Seeder for 5 locations

--- challenge_1/backend/database/seeders/DatabaseSeeder.php
+++ challenge_1/backend/database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(EmployeesTableSeeder::class);
+        $this->call(LocationsTableSeeder::class);
     }
 }
